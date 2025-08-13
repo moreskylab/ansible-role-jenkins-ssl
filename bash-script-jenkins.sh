c830000c55ecbc87768fe5384ee5b1e8ba491859
Ankush More <ankushitguy@gmail.com>
2025-08-14 05:26:48 +0530

    updated instruction for bash script to install-remove jenkins and nginx

--- bash-script-jenkins.sh
+++ bash-script-jenkins.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Install jenkins
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
@@ -6,3 +8,15 @@ echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
 sudo apt-get update
 sudo apt-get install fontconfig openjdk-21-jre
 sudo apt-get install jenkins
+
+# Remove Jenkins
+
+sudo apt-get remove --purge jenkins
+
+############
+
+# Install nginx
+sudo apt-get install nginx
+
+# Remove nginx
+sudo apt-get remove --purge nginx nginx-full nginx-common
